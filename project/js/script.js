7ee7b76ec10d7260c1d783b18602e6e6dc8c0ff9
Dmitriy <trilenko2014@gmail.com>
2020-08-04 21:57:22 +0300

    test commit

---
+++ project/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+//console.log(++incr);
+//console.log(--decr);
+
+//console.log(5%2);
+
+//console.log(2*4 === 8);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || isClose);
