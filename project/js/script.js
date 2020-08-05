a1228951495293a531be95575b50da5a1284844f
Dmitriy <trilenko2014@gmail.com>
2020-08-05 23:49:30 +0300

    Object with options movie

--- project/js/script.js
+++ project/js/script.js
@@ -1,21 +1,12 @@
 "use strict";
 
-//console.log(4 + +'5');
+let numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', 'Количество');
 
-let incr = 10,
-    decr = 10;
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    gengers: Array(0),
+    privat: false
+};
 
-//++incr;
-//--decr;
-
-//console.log(++incr);
-//console.log(--decr);
-
-//console.log(5%2);
-
-//console.log(2*4 === 8);
-
-const isChecked = true,
-      isClose = false;
-
-console.log(isChecked || isClose);
