5e468609e22b00641cc339e2ef6ee05d90432b4c
Dmitriy <trilenko2014@gmail.com>
2020-08-10 00:35:25 +0300

    add count movies you saw

--- project/js/script.js
+++ project/js/script.js
@@ -10,11 +10,21 @@ const personalMovieDB = {
     privat: false
 };
 
+if (personalMovieDB.count <= 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов', 'Название');
     const b = prompt('На сколько оцените его', 'Оценка');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    if (a && b && a.length < 50) {
         personalMovieDB.movies[a] = b;
     } else {
         i--;
